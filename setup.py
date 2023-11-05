840f7d532619ea0df2d0675a7e68eaac57b6dd6d
Maxim Srour <maxim.srour@gmail.com>
2023-11-05 14:16:57 +1100

    Fixed incorrect imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="logger",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     author="Maxim Srour",
     author_email="maxim.srour@gmail.com",
